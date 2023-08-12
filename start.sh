1304eb36de58526906584273bfaf1bce23d48de3
tomas-pucutay <tomas.pucutay@gmail.com>
2023-08-12 12:07:35 -0500

    Fixed start.sh to send .env variables to nginx default.conf due to problems with  recognition

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 export $(cat .env | xargs)
+
 envsubst < config/odoo/odoo.conf.template > config/odoo/odoo.conf
-envsubst < config/nginx/default.conf.template > config/nginx/default.conf
+
+for var in $(grep -oP '\${\w+}' config/nginx/default.conf.template | sort -u); do
+    envsubst "$var" < config/nginx/default.conf.template > config/nginx/default.conf.updated
+    mv config/nginx/default.conf.updated config/nginx/default.conf
+done
+
 sudo docker compose up -d

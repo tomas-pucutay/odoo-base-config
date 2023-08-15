8d2b866fc9b177b3e717565ea2c2f7531dbb18eb
tomas-pucutay <tomas.pucutay@gmail.com>
2023-08-15 15:35:17 -0500

    Manifest and init for crm_custom_fields

---
+++ addons-extra/crm_custom_fields/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'CRM Extension - Custom fields',
+    'version': '16.0.1',
+    'author' : 'tomaspucutay',
+    'website': 'https://github.com/tomas-pucutay',
+    'depends': ['crm'],
+    'data': [],
+    'installable': True
+}

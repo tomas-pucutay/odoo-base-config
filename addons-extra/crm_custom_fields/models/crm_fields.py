fc77bbc8bf9c17aa7a45dd364888b191ae486746
tomas-pucutay <tomas.pucutay@gmail.com>
2023-08-18 17:26:53 -0500

    Test a field type Selection for crm_fields

--- addons-extra/crm_custom_fields/models/crm_fields.py
+++ addons-extra/crm_custom_fields/models/crm_fields.py
@@ -3,7 +3,8 @@ from odoo import models, fields
 class CrmLead(models.Model):
     _inherit = "crm.lead"
 
-    opportunity_type = fields.Char(
+    opportunity_type = fields.Selection(
+        selection = [('a', 'A'), ('b', 'B')],
         string = "Type",
         required = True,
         index = True,
